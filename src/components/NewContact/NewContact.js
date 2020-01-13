5a99950bf7844870cbd33ee287b94f6e67c93d96
ngumina17 <nicole.gumina17@gmail.com>
2020-01-13 10:03:49 -0600

    almost finished

--- src/components/NewContact/NewContact.js
+++ src/components/NewContact/NewContact.js
@@ -4,15 +4,18 @@ class NewContact extends Component {
     render () {
         return {
             <div className='form'>
+            <h1>New Contact</h1>
             <form>
-                <h1>New Contact</h1>
+                
                 <input type='text'
                 <p>Name</p>
                 <p>Email</p>
                 <p>Profile Picture</p>
                 />
             </form>
+
             </div>
+      
 
         }
     }
