1aa8a0e328442cb0fbfa32e3f7266077e0ad35c1
KASSIDY_SHADE <kassie2129@gmail.com>
2020-01-13 09:58:45 -0600

    showing all contacts

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,9 +1,14 @@
 import React from 'react';
 
-function Contact() {
-return(
-<h1>Contact</h1>
-)
+function Contact(props) {
+    console.log(props.person)
+    return (
+        <div className='contact'>
+            <img src={props.person.profile_picture} />
+            <h3>{props.person.name}</h3>
+            <h4>{props.person.email}</h4>
+        </div>
+    )
 }
 
 export default Contact
