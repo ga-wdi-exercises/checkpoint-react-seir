f470c4a60067145f94cf9612b4ef06910c40c0ee
MK16681 <KhanMuhammadT@JohnDeere.com>
2020-01-13 08:03:42 -0800

    changes

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,34 @@
 import React, { Component } from 'react';
+import header from '/.Header'
+import NewContact from '/.NewContact '
+import NewContact from '/.NewContact '
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+      } 
+  from "react-router-dom";
 
 class App extends Component {
   render() {
     return <div className="App">
         <p>app</p>
+<header/>
+<Router>
+<Switch>
+              <Route exact path="/">
+              <ContactList /> 
+              </Route>
+            <Route  path="/new-contact" >
+                <NewContact />
+              </Route>
+            
+           </Switch>
+          
+        </Router>
+
+
       </div>;
   }
 }
--- src/index.js
+++ src/index.js
@@ -2,7 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/index.css';
 import App from './components/App/App';
-
 import contacts from "./contacts.json";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// ReactDOM.render( document.getElementById('root'));
+
+constructor(props) {
+    super(props)
+  
+    this.state = {
+      contacts: props.contacts
+    }
+  }
+class index extends Component {
+    render() {
+      return (
+        <>
+        
+               <App contacts = {contacts}/>,
+        </>
+      );
+    }
+  }
+  export default index;
