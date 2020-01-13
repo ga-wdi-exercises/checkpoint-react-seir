4620178175dc9bd48bd9504f1a581c4ceccee361
Mark Holmes <holmesmarkr@johndeere.com>
2020-01-13 09:56:36 -0600

    checkpoint

--- src/components/NewContact/NewContact.js
+++ src/components/NewContact/NewContact.js
@@ -4,12 +4,7 @@
 function NewContact(){
     console.log()
   }
-  const contacts {
-      name: "Tweety",
-      email: "tweety@gmail.com",
-      profile_picture: "https://upload.wikimedia.org/wikipedia/en/0/02/Tweety.svg"
-    }
-  
+
     return (
       <div className="contact">
         <h1>New Contact</h1>
